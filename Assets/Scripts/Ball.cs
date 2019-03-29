a8f7c7dab1917b5d554b6c11c4ef9791f05e6644
Giorgi Gaiashvili <giorgi.gaiashvili21@gmail.com>
2019-03-29 16:44:29 +0400

    Play Space and Gravity

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,9 +6,13 @@ public class Ball : MonoBehaviour
 {
     // config params
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+   
 
 
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,24 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LaunchOnMouseClick();
+            LockBallToPaddle();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
